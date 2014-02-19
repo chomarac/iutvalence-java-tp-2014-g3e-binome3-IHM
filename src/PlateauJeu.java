40fda1a01a9411c636a500c6c947544981f10c32
Loïc DELORME <Loic.Delorme@iut-valence.fr>
2014-02-19 15:56:18 +0100

    Premier commit

---
+++ src/PlateauJeu.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+
+public class PlateauJeu extends JFrame {
+	
+	public PlateauJeu()
+	{
+		this.setAlwaysOnTop(true);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setTitle("Morpion");
+		this.setSize(10, 1);
+		this.setVisible(true);
+		
+	}
+
+}
