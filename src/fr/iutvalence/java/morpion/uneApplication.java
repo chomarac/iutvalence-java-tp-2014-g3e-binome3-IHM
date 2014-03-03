030cb8fb0e1bb3e37cf1855a93d5e4f24598d4a1
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-03 23:44:09 +0100

    Add TODO (uneApplication.java)

--- src/fr/iutvalence/java/morpion/uneApplication.java
+++ src/fr/iutvalence/java/morpion/uneApplication.java
@@ -12,13 +12,15 @@ public class uneApplication
 	 */
 	public static void main(String[] args)
 	{
+        /* TODO Où est la classe Jouer ? */
 		Jouer unePartie = new Jouer();
 		unePartie.jouer();
 		
 		Scanner recuperationInformation = new Scanner(System.in);
 		System.out.println("Voulez-vous rejouer une partie ? (O/N)");
 		String reponse = recuperationInformation.nextLine();
-		
+
+        /* TODO Il doit manquer une boucle... */
 		if ("O".equals(reponse))
 			unePartie.rejouer();
 	}
