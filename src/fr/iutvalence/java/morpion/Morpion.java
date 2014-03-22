f2dcf3c39e13ca69bf89b5aa58103dc678591029
DelormeLoïc26 <l.delorme@yahoo.fr>
2014-03-22 18:29:49 +0100

    Update

--- src/fr/iutvalence/java/morpion/Morpion.java
+++ src/fr/iutvalence/java/morpion/Morpion.java
@@ -1,25 +1,23 @@
 package fr.iutvalence.java.morpion;
 
-import fr.iutvalence.java.morpion.graphique.Fenetre;
-
 /** Permet d'executer l'application
  * 
  * @author DELORME Loïc & BASSON Julien
  * @version 1.2
  */
-public class UneApplication
+public class Morpion
 {
 	/** Permet de lancer le jeu.
 	 * 
 	 * @param args un argument*/
 	public static void main(String[] args)
 	{
-		/*Controleur controleur;
+		Controleur controleur;
 		do
 		{
 			controleur = new Controleur();
-		} while (controleur.nouvellePartie());*/
-		new Fenetre();
+		} while (controleur.nouvellePartie());
+		//new Fenetre();
 	}
 	
 }
