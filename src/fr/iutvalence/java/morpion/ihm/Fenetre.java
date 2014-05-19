910237585a7250d58518ba6c7db47f28affc2217
Chomarat André <charles-edouard.chomarat@iut-valence.fr>
2014-05-19 15:31:57 +0200

    2 ème séance d'IHM Séparations de la fenêtre finies, menu fini, il ne reste plus que les boutons à assigner.

--- src/fr/iutvalence/java/morpion/ihm/Fenetre.java
+++ src/fr/iutvalence/java/morpion/ihm/Fenetre.java
@@ -6,20 +6,20 @@ package fr.iutvalence.java.morpion.ihm;
  * @version 1.0.0
  */
 
+import java.awt.GridLayout;
+
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.SwingConstants;
 
 public class Fenetre implements Runnable {
-
-	/**Item Partie du menu.*/
-	private JMenuItem menuPartie;
-	
-	/**Item Aide du menu.*/
-	private JMenuItem menuAide;
 	
 	@Override
 	public void run(){
@@ -36,14 +36,80 @@ public class Fenetre implements Runnable {
 		/**Menu de la fenetre*/
 		JMenuBar BarreMenu = new JMenuBar();
 		
+		/**
+		 * Menu "Partie"
+		 */
 		JMenu Partie = new JMenu("Partie");
+		
+		JMenuItem NouvellePartie = new JMenuItem("Nouvelle Partie");
+		
+		JMenuItem Fermer = new JMenuItem("Fermer");
 		
 		JMenu Aide = new JMenu("Aide");
 		
-		BarreMenu.add(Partie);
+		JMenu APropos = new JMenu("A propos");
 		
+		BarreMenu.add(Partie);
+		Partie.add(NouvellePartie);
+		Partie.add(Fermer);
+		
+		BarreMenu.add(Aide);
+		BarreMenu.add(APropos);		
 		fenetre.setJMenuBar(BarreMenu);
 		
+		JSplitPane JSplitPaneVertical = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+		fenetre.getContentPane().add(JSplitPaneVertical);
+		JSplitPaneVertical.setEnabled(false);
+		JSplitPaneVertical.setBorder(null);
+		JSplitPaneVertical.setDividerSize(1);
+		JSplitPaneVertical.setResizeWeight(0.2);
+		
+		JSplitPane JSplitPaneGaucheHorizontal = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		JSplitPaneGaucheHorizontal.setEnabled(false);
+		JSplitPaneGaucheHorizontal.setBorder(null);
+		JSplitPaneGaucheHorizontal.setDividerSize(1);
+		JSplitPaneGaucheHorizontal.setResizeWeight(0.5);
+		
+		JSplitPane JSplitPaneDroiteHorizontal = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		JSplitPaneDroiteHorizontal.setEnabled(false);
+		JSplitPaneDroiteHorizontal.setBorder(null);
+		JSplitPaneDroiteHorizontal.setDividerSize(1);
+		JSplitPaneDroiteHorizontal.setResizeWeight(0.1);
+
+		JSplitPaneVertical.add(JSplitPaneGaucheHorizontal);
+		
+		JSplitPaneVertical.add(JSplitPaneDroiteHorizontal);
+			
+	    Box BoxJoueur1 = Box.createHorizontalBox();
+		
+		JSplitPaneGaucheHorizontal.add(BoxJoueur1);
+		
+		BoxJoueur1.add(new JLabel("Joueur 1"));
+		
+		Box BoxJoueur2 = Box.createHorizontalBox();
+		
+		JSplitPaneGaucheHorizontal.add(BoxJoueur2);
+		
+		BoxJoueur2.add(new JLabel("Joueur 2"));
+		
+		JSplitPaneDroiteHorizontal.add(new JLabel("C'est à joueur x de jouer."));
+		
+	//	JSplitPaneDroiteHorizontal.setAlignmentX(SwingConstants.CENTER);
+		
+		JPanel morpion = new JPanel();
+		morpion.setLayout(new GridLayout(3,3));
+		
+		JSplitPaneDroiteHorizontal.add(morpion);
+		
+		morpion.add(new JButton());
+		morpion.add(new JButton());
+		morpion.add(new JButton());
+		morpion.add(new JButton());
+		morpion.add(new JButton());
+		morpion.add(new JButton());
+		morpion.add(new JButton());
+		morpion.add(new JButton());
+		morpion.add(new JButton());
 		
 		
 	}
